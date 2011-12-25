505889fbaf35d91ffdbcb0a7f5250f593d2a1660
Huey Kwik <huey.kwik@gmail.com>
2011-12-24 22:26:16 -0800

    Hardcoded git log to use the last 2 weeks.

--- codedelta.py
+++ codedelta.py
@@ -6,47 +6,48 @@ import re
 num_args = len(sys.argv)
 
 if num_args != 2:
-	print """
+    print """
 Usage: codedelta <gitpath..>
 """
-	sys.exit(2);
-
+    sys.exit(2);
+        
 gitpath = sys.argv[1]
 os.chdir(gitpath)
 
-#author->dict(ins, removed)
-authorToCode = {}
+# Map of author to number of inserted and deleted lines of code.
+authorToCode = {} 
 
-# get the list of authors
-authors = subprocess.Popen("git log --format='%aN' | sort -u", shell=True, stdout=subprocess.PIPE)
+# Get the list of authors.
+authors = subprocess.Popen("git log --format='%aN' | sort -u", 
+                            shell=True, stdout=subprocess.PIPE)
 authorList = authors.communicate()[0].split('\n')
+
+# For each author, compute his/her number of lines of code.
+# Assumed that authorList is ordered and each element is unique.
 for author in authorList:
-	if len(author) == 0:
-		continue
-	command = 'git log --author=\"' +author+ '\" --pretty=tformat: --numstat'
+    if len(author) == 0:
+        continue
+    command = 'git log --since=2.weeks --author=\"' + author + '\" --pretty=tformat: --numstat'
 
-	# for each author, get their stats
-	stats = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
-	statLines = stats.communicate()[0].split('\n')
-	
-	for line in statLines:
-		if len (line) == 0:
-			continue
-		elements = line.split()
-		
-		try: 
-			insertions = int(elements[0])
-			deletions = int(elements[1])
-		except ValueError:
-			# if it's not an integer, just punt
-			continue
+    stats = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
+    statLines = stats.communicate()[0].split('\n')
+    
+    authorToCode[author] = {'insertions': 0, 'deletions': 0}
 
-		if author in authorToCode: 
-			authorToCode[author]['insertions'] += insertions
-			authorToCode[author]['deletions'] += deletions
-		else:
-			codeDelta = {'insertions': insertions, 'deletions': deletions}
-			authorToCode[author] = codeDelta
-	total = authorToCode[author]['insertions'] + authorToCode[author]['deletions']
-	print '{} {} {} {}'.format(author, total, insertions, deletions)
-	
+    for line in statLines:
+        if len(line) == 0:
+            continue
+        elements = line.split()
+        
+        try: 
+            insertions = int(elements[0])
+            deletions = int(elements[1])
+        except ValueError:
+            continue # Punt if there's a parse error.
+ 
+        authorToCode[author]['insertions'] += insertions
+        authorToCode[author]['deletions'] += deletions
+        
+    total = authorToCode[author]['insertions'] + authorToCode[author]['deletions']
+    print '{} {} {} {}'.format(author, total, insertions, deletions)
+    
